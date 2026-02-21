8acc464794a082e5899dd7fecd5fbe6796242bf0
waney <ssdii66@gmail.com>
2026-02-21 21:26:59 +0900

    [java_adv3] section3.람다.람다의 전달

---
+++ src/lambda/lambda2/LambdaPassMain3.java
@@ -0,0 +1,32 @@
+package lambda.lambda2;
+
+import lambda.MyFunction;
+
+public class LambdaPassMain3 {
+    public static void main(String[] args) {
+        MyFunction add = getOperation("add");
+        MyFunction sub = getOperation("sub");
+        MyFunction mul = getOperation("mul");
+        MyFunction div = getOperation("div");
+
+        System.out.println("add: "+ add.apply(1,2));
+        System.out.println("sub: "+ sub.apply(1,2));
+        System.out.println("mul: "+ mul.apply(1,2));
+        System.out.println("div: "+ div.apply(1,2));
+
+
+    }
+    static MyFunction getOperation(String operator) {
+        switch (operator) {
+            case "add":
+                return (a, b) -> a + b;
+                case "sub":
+                    return (a, b) -> a - b;
+                    case "mul":
+                        return (a, b) -> a * b;
+                        case "div":
+                            return (a, b) -> a / b;
+        };
+        return null;
+    };
+}
